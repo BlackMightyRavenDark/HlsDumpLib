f6ab75a633381b38ff8311b8f77be291589c78f8
BlackRain <masaraksh86@mail.ru>
2023-07-02 13:50:32 +0700

    Добавьте файлы проекта.

---
+++ HlsDumpLib.ConsoleTest/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+
+namespace HlsDumpLib.ConsoleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] inputBuffer = new byte[8192];
+            Stream inputStream = Console.OpenStandardInput(inputBuffer.Length);
+            Console.SetIn(new StreamReader(inputStream, Console.InputEncoding, false, inputBuffer.Length));
+
+            Console.WriteLine("Enter HLS-playlist URL (M3U8). Warning! Not all playlist formats are supported yet!");
+            Console.Write("URL: ");
+            string url = Console.ReadLine();
+            if (!string.IsNullOrEmpty(url) && !string.IsNullOrWhiteSpace(url))
+            {
+                int errorCode = MultiThreadedDownloaderLib.FileDownloader.GetUrlResponseHeaders(url, out _, out string errorText);
+                if (errorCode == 200)
+                {
+                    HlsDumper dumper = new HlsDumper(url);
+                    dumper.Dump(OnPlaylistChecking, OnNextFile, OnWarning, OnError, OnFinished);
+                }
+                else
+                {
+                    Console.WriteLine($"Error {errorCode}! {errorText}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Empty URL! Exiting... Press any key...");
+            }
+            Console.ReadLine();
+        }
+
+        private static void OnPlaylistChecking(object sender, string fileUrl)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Checking playlist: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(fileUrl);
+        }
+
+        private static void OnNextFile(object sender, string fileUrl)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Next file: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(fileUrl);
+        }
+
+        private static void OnError(object sender, string message, int errorCount)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Error {errorCount} / 5: {message}");
+        }
+
+        private static void OnWarning(object sender, string message, int errorCount)
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine($"Error {errorCount} / 5: {message}");
+        }
+
+        private static void OnFinished(object sender)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Dump is finished");
+        }
+    }
+}
---
+++ HlsDumpLib/HlsDumper.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MultiThreadedDownloaderLib;
+
+namespace HlsDumpLib
+{
+    public class HlsDumper
+    {
+        public string Url { get; }
+        private readonly LinkedList<string> _fileList = new LinkedList<string>();
+
+        public HlsDumper(string url)
+        {
+            Url = url;
+        }
+
+        public async void Dump(
+            Action<object, string> playlistChecking,
+            Action<object, string> nextFile,
+            Action<object, string, int> warning,
+            Action<object, string, int> error,
+            Action<object> finished)
+        {
+            FileDownloader playlistDownloader = new FileDownloader() { Url = Url };
+            await Task.Run(() =>
+            {
+                int errorCount = 0;
+                do
+                {
+                    playlistChecking?.Invoke(this, Url);
+                    int errorCode = playlistDownloader.DownloadString(out string response);
+                    if (errorCode == 200)
+                    {
+                        List<string> list = FilterPlaylist(ExtractUrlsFromPlaylist(response));
+                        if (list != null && list.Count > 0)
+                        {
+                            foreach (string item in list)
+                            {
+                                _fileList.AddLast(item);
+                                if (_fileList.Count > 50)
+                                {
+                                    _fileList.RemoveFirst();
+                                }
+                                nextFile?.Invoke(this, item);
+                                errorCount = 0;
+                            }
+                        }
+                        else
+                        {
+                            errorCount++;
+                            warning?.Invoke(this, "No new files detected", errorCount);
+                        }
+                    }
+                    else
+                    {
+                        errorCount++;
+                        error?.Invoke(this, "Playlist is not found", errorCount);
+                    }
+
+                    Thread.Sleep(2000);
+                } while (errorCount < 5);
+            });
+
+            finished?.Invoke(this);
+        }
+
+        private List<string> ExtractUrlsFromPlaylist(string playlist)
+        {
+            List<string> result = new List<string>();
+            string[] strings = playlist.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+            result.AddRange(strings.Where(s => s.StartsWith("http")));
+            return result;
+        }
+
+        private List<string> FilterPlaylist(List<string> playlist)
+        {
+            return playlist.Where(s => !_fileList.Contains(s))?.ToList();
+        }
+    }
+}
